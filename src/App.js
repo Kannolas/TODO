74853bca354e709547ca0bc3f055c7dbdeae3e6c
Kannolas <vanya.pokorsky@gmail.com>
2023-06-03 15:54:35 +0300

    added notifications

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import TodoContainer from "./components/TodoContainer";
 import './styles.css'
 
 function App() {
+  const notif = document.querySelectorAll(".added")
   const [todos, setTodos]= useState([])
+  const[isShowAdded, setShowAdded] = useState(false)
+  const [isDeleted, setIsDeleted] = useState(false)
 
   const AddTask = (input)=>{
     if(input){
@@ -15,11 +18,37 @@ function App() {
         completed:false
       }
       setTodos([...todos, newItem])
+      setShowAdded(true)
+      setTimeout(()=>{
+        setShowAdded(false)
+      }, 2000)
     }
   }
 
+  useEffect(()=>{
+    if(isShowAdded){
+      notif[0].classList.add("showadded")
+    }
+    else{
+      notif[0].classList.remove("showadded")
+    }
+    if(isDeleted){
+      notif[1].classList.add("showadded")
+    }
+    else{
+      notif[1].classList.remove("showadded")
+    }
+  })
+
+
+
   const DeleteTask = (id)=>{
     setTodos([...todos.filter((todo)=>todo.id !== id)])
+    setIsDeleted(true)
+      setTimeout(()=>{
+        setIsDeleted(false)
+      }, 2000)
+
   }
 
   const ToggleTask=  ()=>{
@@ -29,10 +58,16 @@ function App() {
     <div className="App">
       <header><h1>Количество задач: {todos.length}</h1></header>
       <TodoContainer add={AddTask}/>
-      {todos.map((todo)=>{
+      {todos.reverse().map((todo)=>{
       return(
         <TodoItem todo = {todo} id = {todo.id} remove={DeleteTask} toggleT={ToggleTask}/>
       )})}
+      <div className="added">
+        <div className="added-text">Successfully added</div>
+      </div>
+      <div className="added deleted">
+        <div className="added-text">Deleted</div>
+      </div>
     </div>
   );
 }
